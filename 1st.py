db2cdfea2a294e2bda18fc616fdf7ab705e6c5d0
zenlaboratory <zenlaboratory@gmail.com>
2022-11-03 02:53:45 +0100

    2nd

--- 1st.py
+++ 1st.py
@@ -1,4 +1,34 @@
 # Esto es un comentario
 
-print("Hello!")
-print(89)
+""" Y esto otro 
+comentario mutilinea """
+
+# Variables
+
+num_a = 4 # int
+num_b = 32.5 # float. Con parte decimal separada por punto.
+nombre = "Carlos" # str
+joven = True # bool
+numero_complejo = 4+2j # complex
+
+# Conocemos el tipo de dato con type()
+# Imprimimos con print
+
+print(type(num_a))
+print(type(num_b))
+print(type(nombre))
+print(type(joven))
+print(type(numero_complejo))
+
+# Operaciones con str
+
+# Obtenemos la longitud del string con len()
+
+print(len(nombre))
+
+# Obtenemos el valor de la posicion (index) del str[0..n]
+
+print(nombre[2])
+
+
+    
